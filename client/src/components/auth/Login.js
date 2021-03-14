298531a5555480dccc96b1a411ea34cd41246531
Kishan Patel <kishangp97@gmail.com>
2021-03-14 11:47:40 +0530

    navbar signup signin fixed

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,46 +1,51 @@
 import React from "react";
-
+import Alert2 from "../layout/Alert2";
 export default function Signup2() {
   return (
     <>
       <div className="login-form">
-        <form method="post">
-          <h2 class="text-center">Log In</h2>
+        <div>
+          <Alert2 />
+          <form method="post">
+            <h1 className="text-center">Log In</h1>
 
-          <div class="form-group">
-            <input
-              type="email"
-              class="form-control"
-              placeholder="Email"
-              required="required"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="password"
-              class="form-control"
-              placeholder="Password"
-              required="required"
-            />
-          </div>
-          {/* <div class="form-group">
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                required="required"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                required="required"
+              />
+            </div>
+            {/* <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block">
               Sign Up
             </button>
           </div> */}
-          <div className="text-center ">
-            <button
-              type="submit"
-              className="btn cycle-btn"
-              style={{ width: "160px" }}
-            >
-              Log In
-            </button>
-          </div>
-        </form>
-        <p class="text-center">
-          <a href="#">Sign In here</a>
-        </p>
+            <div className="text-center ">
+              <button
+                type="submit"
+                className="btn cycle-btn "
+                style={{ width: "160px" }}
+              >
+                Log In
+              </button>
+            </div>
+          </form>
+          <p className="text-center t-blue">
+            <a href="#" className="t-blue font-weight-bold">
+              Sign In here
+            </a>
+          </p>
+        </div>
       </div>
     </>
   );
---
+++ client/src/components/layout/Navbar2.js
@@ -0,0 +1,68 @@
+import React from "react";
+import logo from "../files/images/mybyc_logo.png";
+
+export default function Navbar2() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        {/* <a className="navbar-brand" href="#">
+          Navbar
+        </a> */}
+        <img src={logo} className="img-fluid mr-5" alt="manin" width="50px" />
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link font-large hovered" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-large hovered" href="#">
+                About Us
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link font-large hovered" href="#">
+                Booking
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <div className="text-center ">
+              <button
+                type="submit"
+                className="btn cycle-btn"
+                style={{ width: "130px" }}
+              >
+                Sign Up
+              </button>
+            </div>
+            <div className="text-center mx-3 ">
+              <button
+                type="submit"
+                className="btn cycle-btn"
+                style={{ width: "130px" }}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+}
